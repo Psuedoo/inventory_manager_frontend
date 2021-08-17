c3ca64e00c26bb646ea9c9ca01658ac3a24add28
Psuedo <forthewin720@gmail.com>
2021-08-17 19:46:30 -0400

    dynamic list vieweing of computers

--- index.js
+++ index.js
@@ -1,14 +1,18 @@
+Vue.component('computer-item', {
+    props: ['computer'],
+    template: '<li>Make: {{ computer.make }} Model: {{ computer.model }} Asset Tag: {{ computer.asset_tag }} Service Tag: {{ computer.service_tag }}</li>'
+})
+
 var app = new Vue({
     el: '#app',
-    data () {
-        return {
-            info: null
-        }
+    data: {
+        computerList: [
+        ]
     },
     mounted () {
         axios
         .get('http://localhost:8000/inventory/?make=Dell')
-        .then(response => (this.info = response))
+        .then(response => (this.computerList = response['data']))
     }
 })
 
