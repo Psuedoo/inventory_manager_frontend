81f3eb539e73930ae2a87f9a1992f0d29fd18d9a
Psuedoo <forthewin720@gmail.com>
2021-08-17 15:01:16 -0400

    trying to get data from endpoint

--- index.js
+++ index.js
@@ -1,6 +1,14 @@
 var app = new Vue({
     el: '#app',
-    data: {
-        message: 'Hello Vue!'
+    data () {
+        return {
+            info: null
+        }
+    },
+    mounted () {
+        axios
+        .get('http://localhost:8000/inventory/?asset_tag=125700')
+        .then(response => (this.info = response))
     }
 })
+
