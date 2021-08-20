a89e0e6c7c2e88ade1068c05fb4ed7eaa782622e
Psuedoo <forthewin720@gmail.com>
2021-08-20 10:30:18 -0400

    computer form works to submit data

--- add-computer.js
+++ add-computer.js
@@ -9,7 +9,7 @@ Vue.component('add-computer-form',{
             assigned_to: '',
             on_hand: false,
             on_location: false,
-            location: '',
+            computer_location: '',
             class_location: '',
             checker: '',
             notes: '',
@@ -27,7 +27,7 @@ Vue.component('add-computer-form',{
                 assigned_to: this.assigned_to,
                 on_hand: this.on_hand,
                 on_location: this.on_location,
-                location: this.location,
+                computer_location: this.computer_location,
                 class_location: this.class_location,
                 checker: this.checker,
                 notes: this.notes
@@ -39,45 +39,45 @@ Vue.component('add-computer-form',{
     template:`
     <div>
         <slot></slot>
-        <form action="javascript:void(0)">
+        <form @submit.prevent="postComputer" action="" method="post">
         <p>
-        <input type="text" name="fmake" placeholder="Make" v-model="this.make">
+        <input type="text" name="fmake" placeholder="Make" v-model="make">
         </p>
         <p>
-        <input type="text" name="fmodel" placeholder="Model" v-model="this.model">
+        <input type="text" name="fmodel" placeholder="Model" v-model="model">
         </p>
         <p>
-        <input type="text" name="fserviceTag" placeholder="Service Tag" v-model="this.service_tag">
+        <input type="text" name="fserviceTag" placeholder="Service Tag" v-model="service_tag">
         </p>
         <p>
-        <input type="text" name="fassetTag" placeholder="Asset Tag" v-model="this.asset_tag">
+        <input type="text" name="fassetTag" placeholder="Asset Tag" v-model="asset_tag">
         </p>
         <p>
-        <input type="checkbox" name="fissued" placeholder="Issued" v-model="this.issued">
+        <input type="checkbox" name="fissued" placeholder="Issued" v-model="issued">
         </p>
         <p>
-        <input type="text" name="fassignedTo" placeholder="Assigned" v-model="this.assigned_to">
+        <input type="text" name="fassignedTo" placeholder="Assigned" v-model="assigned_to">
         </p>
         <p>
-        <input type="checkbox" name="fonHand" placeholder="On Hand" v-model="this.on_hand">
+        <input type="checkbox" name="fonHand" placeholder="On Hand" v-model="on_hand">
         </p>
         <p>
-        <input type="checkbox" name="fonLocation" placeholder="On Location" v-model="this.on_location">
+        <input type="checkbox" name="fonLocation" placeholder="On Location" v-model="on_location">
         </p>
         <p>
-        <input type="text" name="flocation" placeholder="Location" v-model="this.location">
+        <input type="text" name="fcomputerLocation" placeholder="Location" v-model="computer_location">
         </p>
         <p>
-        <input type="text" name="fclassLocation" placeholder="Class Location" v-model="this.class_location">
+        <input type="text" name="fclassLocation" placeholder="Class Location" v-model="class_location">
         </p>
         <p>
-        <input type="text" name="fchecker" placeholder="Checker" v-model="this.checker">
+        <input type="text" name="fchecker" placeholder="Checker" v-model="checker">
         </p>
         <p>
-        <input type="textarea" name="fnotes" placeholder="Notes" v-model="this.notes">
+        <input type="textarea" name="fnotes" placeholder="Notes" v-model="notes">
         </p>
         <p>
-        <input type="submit" v-on:click="postComputer()">
+        <input type="submit">
         </p>
         </form>
     </div>
@@ -91,20 +91,6 @@ var app = new Vue({
         loading: true,
         errored: false,
         postSuccess: '',
-        parentComputer: {
-            make: '',
-            model: '',
-            service_tag: '',
-            asset_tag: '',
-            issued: false,
-            assigned_to: '',
-            on_hand: false,
-            on_location: false,
-            location: '',
-            class_location: '',
-            checker: '',
-            notes: '',
-        }
     },
     methods: {
 
