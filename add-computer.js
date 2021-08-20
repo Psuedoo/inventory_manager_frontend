8656f56e3d063fd1c1da1123254b5afa0bd74c3d
Psuedo <forthewin720@gmail.com>
2021-08-19 22:29:00 -0400

    trying to send post request with form

--- add-computer.js
+++ add-computer.js
@@ -1,47 +1,83 @@
 Vue.component('add-computer-form',{
-    props: ['parent'],
+    data : function () {
+        return {
+            make: '',
+            model: '',
+            service_tag: '',
+            asset_tag: '',
+            issued: false,
+            assigned_to: '',
+            on_hand: false,
+            on_location: false,
+            location: '',
+            class_location: '',
+            checker: '',
+            notes: '',
+        }
+    },
+    methods: {
+        postComputer: function () {
+            axios
+            .post('http://localhost:8000/inventory/add/', {
+                make: this.make,
+                model: this.model,
+                service_tag: this.service_tag,
+                asset_tag: this.asset_tag,
+                issued: this.issued,
+                assigned_to: this.assigned_to,
+                on_hand: this.on_hand,
+                on_location: this.on_location,
+                location: this.location,
+                class_location: this.class_location,
+                checker: this.checker,
+                notes: this.notes
+
+            })
+            // .then(response => (parent.response = response.statusCode))
+        }    
+    },
     template:`
     <div>
         <slot></slot>
-        <form action="index.html">
+        <form action="javascript:void(0)">
         <p>
-        <input type="text" name="fmake" placeholder="Make" v-model="parent.make">
+        <input type="text" name="fmake" placeholder="Make" v-model="this.make">
         </p>
         <p>
-        <input type="text" name="fmodel" placeholder="Model" v-model="parent.model">
+        <input type="text" name="fmodel" placeholder="Model" v-model="this.model">
         </p>
         <p>
-        <input type="text" name="fserviceTag" placeholder="Service Tag" v-model="parent.serviceTag">
+        <input type="text" name="fserviceTag" placeholder="Service Tag" v-model="this.service_tag">
         </p>
         <p>
-        <input type="text" name="fassetTag" placeholder="Asset Tag" v-model="parent.assetTag">
+        <input type="text" name="fassetTag" placeholder="Asset Tag" v-model="this.asset_tag">
         </p>
         <p>
-        <input type="checkbox" name="fissued" placeholder="Issued" v-model="parent.issued">
+        <input type="checkbox" name="fissued" placeholder="Issued" v-model="this.issued">
         </p>
         <p>
-        <input type="text" name="fassignedTo" placeholder="Assigned" v-model="parent.assignedTo">
+        <input type="text" name="fassignedTo" placeholder="Assigned" v-model="this.assigned_to">
         </p>
         <p>
-        <input type="checkbox" name="fonHand" placeholder="On Hand" v-model="parent.onHand">
+        <input type="checkbox" name="fonHand" placeholder="On Hand" v-model="this.on_hand">
         </p>
         <p>
-        <input type="checkbox" name="fonLocation" placeholder="On Location" v-model="parent.onLocation">
+        <input type="checkbox" name="fonLocation" placeholder="On Location" v-model="this.on_location">
         </p>
         <p>
-        <input type="text" name="flocation" placeholder="Location" v-model="parent.location">
+        <input type="text" name="flocation" placeholder="Location" v-model="this.location">
         </p>
         <p>
-        <input type="text" name="fclassLocation" placeholder="Class Location" v-model="parent.classLocation">
+        <input type="text" name="fclassLocation" placeholder="Class Location" v-model="this.class_location">
         </p>
         <p>
-        <input type="text" name="fchecker" placeholder="Checker" v-model="parent.checker">
+        <input type="text" name="fchecker" placeholder="Checker" v-model="this.checker">
         </p>
         <p>
-        <input type="textarea" name="fnotes" placeholder="Notes" v-model="parent.notes">
+        <input type="textarea" name="fnotes" placeholder="Notes" v-model="this.notes">
         </p>
         <p>
-        <input type="submit">
+        <input type="submit" v-on:click="postComputer()">
         </p>
         </form>
     </div>
@@ -70,23 +106,9 @@ var app = new Vue({
             notes: '',
         }
     },
-    mounted () {
-        axios
-        .post('http://localhost:8000/inventory/', this.parentComputer)
-        .then(response => (this.response = response.statusCode))
-        // .catch(error => {
-        //     console.log(error)
-        //     this.errored = true
-        // })
-        // .finally(() => this.loading = false)
+    methods: {
+
     },
-    computed: {
-        filteredList(){
-            return this.computerList.filter(computer => {
-                return computer[this.parentSearch.property].includes(this.parentSearch.search)
-            })
-        }
-    }
 })
 
 Vue.prototype.moment = moment;
