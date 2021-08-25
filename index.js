ba0178125e3d491fbbd4c4cd7ba9526b82b55a1b
Psuedo <forthewin720@gmail.com>
2021-08-24 21:59:15 -0400

    changed navbar add computer to a nav item and used pills

--- index.js
+++ index.js
@@ -17,7 +17,18 @@ Vue.component('search', {
             <option value="notes">Notes</option>
         </select>
         
-        <input type="text" v-model="tableFilter.search" placeholder="Type here">
+        <li class="nav-item dropdown">
+        <a class="nav-link dropdown-toggle" data-bs-toggle="dropdown" href="#" role="button" aria-expanded="false">Dropdown</a>
+        <ul class="dropdown-menu">
+            <li><a class="dropdown-item" href="#">Action</a></li>
+            <li><a class="dropdown-item" href="#">Another action</a></li>
+            <li><a class="dropdown-item" href="#">Something else here</a></li>
+            <li><hr class="dropdown-divider"></li>
+            <li><a class="dropdown-item" href="#">Separated link</a></li>
+            </ul>
+        </li>
+
+        <input class="form-control me-2" type="search" v-model="tableFilter.search" placeholder="Type here">
     </div>
     `
 })
