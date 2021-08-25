d980c9f6ec6529e6c94ae247b4cf2bdfb765b902
Psuedoo <forthewin720@gmail.com>
2021-08-25 09:23:59 -0400

    removed dropdown on navbar

--- index.js
+++ index.js
@@ -17,17 +17,6 @@ Vue.component('search', {
             <option value="notes">Notes</option>
         </select>
         
-        <li class="nav-item dropdown">
-        <a class="nav-link dropdown-toggle" data-bs-toggle="dropdown" href="#" role="button" aria-expanded="false">Dropdown</a>
-        <ul class="dropdown-menu">
-            <li><a class="dropdown-item" href="#">Action</a></li>
-            <li><a class="dropdown-item" href="#">Another action</a></li>
-            <li><a class="dropdown-item" href="#">Something else here</a></li>
-            <li><hr class="dropdown-divider"></li>
-            <li><a class="dropdown-item" href="#">Separated link</a></li>
-            </ul>
-        </li>
-
         <input class="form-control me-2" type="search" v-model="tableFilter.search" placeholder="Type here">
     </div>
     `
