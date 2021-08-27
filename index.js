eb551ea8a1b8a27141a748a55740ecf2aeeb81e8
Psuedoo <forthewin720@gmail.com>
2021-08-27 16:30:48 -0400

    removed unneccessary comments

--- index.js
+++ index.js
@@ -419,47 +419,6 @@ Vue.component('computer-table', {
 
 })
 
-// Vue.component('app-bar', {
-//     template:`
-// <template>
-//   <v-card class="overflow-hidden">
-//     <v-app-bar
-//       absolute
-//       color="white"
-//       elevate-on-scroll
-//       scroll-target="#scrolling-techniques-7"
-//     >
-//       <v-app-bar-nav-icon></v-app-bar-nav-icon>
-
-//       <v-toolbar-title>Inventory</v-toolbar-title>
-
-//       <v-spacer></v-spacer>
-
-//       <v-btn icon>
-//         <v-icon>mdi-magnify</v-icon>
-//       </v-btn>
-
-//       <v-btn icon>
-//         <v-icon>mdi-heart</v-icon>
-//       </v-btn>
-
-//       <v-btn icon>
-//         <v-icon>mdi-dots-vertical</v-icon>
-//       </v-btn>
-//     </v-app-bar>
-//     <v-sheet
-//       id="scrolling-techniques-7"
-//       class="overflow-y-auto"
-//       max-height="600"
-//     >
-//       <v-container style="height: 60px;">
-//       </v-container>
-//     </v-sheet>
-//   </v-card>
-// </template>
-//     `
-// })
-
 
 var app = new Vue({
     el: '#app',
