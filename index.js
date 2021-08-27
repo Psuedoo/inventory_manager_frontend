6ce5a98cdcd158d8c49a2a2cd3c46419bd5e6ae1
Psuedoo <forthewin720@gmail.com>
2021-08-27 15:20:15 -0400

    can add/edit/delete computers to the TABLE; not the database yet

--- index.js
+++ index.js
@@ -2,7 +2,8 @@ Vue.component('computer-table', {
     props: ['computers'],
     data: function () {
         return {
-            search: '',
+            dialog: false,
+            dialogDelete: false,
             headers: [
             {
                 text: 'Make',
@@ -22,37 +23,342 @@ Vue.component('computer-table', {
             { text: 'Checker', value: 'checker' },
             { text: 'Last Time Checked', value: 'time_checked' },
             { text: 'Notes', value: 'notes' },
+            { text: 'Actions', value:'actions', sortable: 'false' }
         ],
+        editedIndex: -1,
+        editedItem: {
+            make: '',
+            model: '',
+            service_tag: '',
+            asset_tag: '',
+            issued: false,
+            assigned_to: '',
+            on_hand: true,
+            on_location: false,
+            computer_location: '',
+            class_location: '',
+            checker: '',
+            time_checked: '',
+            notes: ''
+        },
+        defaultItem: {
+            make: '',
+            model: '',
+            service_tag: '',
+            asset_tag: '',
+            issued: false,
+            assigned_to: '',
+            on_hand: true,
+            on_location: false,
+            computer_location: '',
+            class_location: '',
+            checker: '',
+            time_checked: '',
+            notes: ''
         }
-    },
+        }},
+
+        computed: {
+            formTitle () {
+                return this.editedIndex === -1 ? 'New Computer' : 'Edit Computer'
+            }
+        },
+
+        watch: {
+            dialog (val) {
+                val || this.close()
+            },
+            dialogDelete (val) {
+                val || this.closeDelete()
+            }
+        },
+
+        methods: {
+            formatDate(value) {
+                return moment(value).fromNow()
+            },
+            getColor(time) {
+                var timeObj = moment(time);
+                var now = moment();
+                var duration = moment.duration(now.diff(timeObj, 'days'));
+                if (duration > 30) return 'red'
+                if (duration > 15) return 'yellow'
+                else return 'green'
+            },
+            editItem (item) {
+                this.editedIndex = this.computers.indexOf(item)
+                this.editedItem = Object.assign({}, item)
+                this.dialog = true
+            },
+            deleteItem (item) {
+                this.editedIndex = this.computers.indexOf(item)
+                this.editedItem = Object.assign({}, item)
+                this.dialogDelete = true
+            },
+            deleteItemConfirm () {
+                this.computers.splice(this.editedIndex, 1)
+                this.closeDelete()
+            },
+            close () {
+                this.dialog = false
+                this.$nextTick(() => {
+                    this.editedItem = Object.assign({}, this.defaultItem)
+                    this.editedIndex = -1
+                })
+            },
+            closeDelete () {
+                this.dialogDelete = false
+                this.$nextTick(() => {
+                    this.editedItem = Object.assign({}, this.defaultItem)
+                    this.editedIndex = -1
+                })
+            },
+            save () {
+                if (this.editedIndex > -1) {
+                    Object.assign(this.computers[this.editedIndex], this.editedItem)
+                } else {
+                    this.computers.push(this.editedItem)
+                }
+                this.close()
+            }
+        },
+
     template: `
     <div>
         <v-data-table
         :headers="this.headers"
         :items="this.computers"
-        :search="search"
         :items-per-page="1500"
         class="elevation-1"
         loading-text="Loading... Please wait"
         no-data-text="No data"
         >
+            <template v-slot:top>
+            <v-toolbar
+            flat
+            >
+            <v-toolbar-title>Computers</v-toolbar-title>
+            <v-divider
+                class="mx-4"
+                inset
+                vertical
+            ></v-divider>
+            <v-spacer></v-spacer>
+            <v-dialog
+                v-model="dialog"
+                max-width="500px"
+            >
+                <template v-slot:activator="{ on, attrs }">
+                <v-btn
+                    color="primary"
+                    dark
+                    class="mb-2"
+                    v-bind="attrs"
+                    v-on="on"
+                >
+                    New Item
+                </v-btn>
+                </template>
+                <v-card>
+                <v-card-title>
+                    <span class="text-h5">{{ formTitle }}</span>
+                </v-card-title>
+    
+                <v-card-text>
+                    <v-container>
+                    <v-row>
+                        <v-col
+                        cols="12"
+                        sm="6"
+                        md="4"
+                        >
+                        <v-text-field
+                            v-model="editedItem.make"
+                            label="Make"
+                        ></v-text-field>
+                        </v-col>
+                        <v-col
+                        cols="12"
+                        sm="6"
+                        md="4"
+                        >
+                        <v-text-field
+                            v-model="editedItem.model"
+                            label="Model"
+                        ></v-text-field>
+                        </v-col>
+                        <v-col
+                        cols="12"
+                        sm="6"
+                        md="4"
+                        >
+                        <v-text-field
+                            v-model="editedItem.service_tag"
+                            label="Service Tag"
+                        ></v-text-field>
+                        </v-col>
+                        <v-col
+                        cols="12"
+                        sm="6"
+                        md="4"
+                        >
+                        <v-text-field
+                            v-model="editedItem.asset_tag"
+                            label="Asset Tag"
+                        ></v-text-field>
+                        </v-col>
+                        <v-col
+                        cols="12"
+                        sm="6"
+                        md="4"
+                        >
+                        <v-checkbox
+                            v-model="editedItem.issued"
+                            label="Issued"
+                        ></v-checkbox>
+                        </v-col>
+                        <v-col
+                        cols="12"
+                        sm="6"
+                        md="4"
+                        >
+                        <v-text-field
+                            v-model="editedItem.assigned_to"
+                            label="Assigned To"
+                        ></v-text-field>
+                        </v-col>
+                        <v-col
+                        cols="12"
+                        sm="6"
+                        md="4"
+                        >
+                        <v-checkbox
+                            v-model="editedItem.on_hand"
+                            label="On Hand"
+                        ></v-checkbox>
+                        </v-col>
+                        <v-col
+                        cols="12"
+                        sm="6"
+                        md="4"
+                        >
+                        <v-checkbox
+                            v-model="editedItem.on_location"
+                            label="On Location"
+                        ></v-checkbox>
+                        </v-col>
+                        <v-col
+                        cols="12"
+                        sm="6"
+                        md="4"
+                        >
+                        <v-text-field
+                            v-model="editedItem.computer_location"
+                            label="Location"
+                        ></v-text-field>
+                        </v-col>
+                        <v-col
+                        cols="12"
+                        sm="6"
+                        md="4"
+                        >
+                        <v-text-field
+                            v-model="editedItem.class_location"
+                            label="Class Location"
+                        ></v-text-field>
+                        </v-col>
+                        <v-col
+                        cols="12"
+                        sm="6"
+                        md="4"
+                        >
+                        <v-text-field
+                            v-model="editedItem.checker"
+                            label="Checker"
+                        ></v-text-field>
+                        </v-col>
+                        <v-col
+                        cols="12"
+                        sm="6"
+                        md="4"
+                        >
+                        <v-text-field
+                            v-model="editedItem.notes"
+                            label="Notes"
+                        ></v-text-field>
+                        </v-col>
+                    </v-row>
+                    </v-container>
+                </v-card-text>
+    
+                <v-card-actions>
+                    <v-spacer></v-spacer>
+                    <v-btn
+                    color="blue darken-1"
+                    text
+                    @click="close"
+                    >
+                    Cancel
+                    </v-btn>
+                    <v-btn
+                    color="blue darken-1"
+                    text
+                    @click="save"
+                    >
+                    Save
+                    </v-btn>
+                </v-card-actions>
+                </v-card>
+            </v-dialog>
+            <v-dialog v-model="dialogDelete" max-width="500px">
+                <v-card>
+                <v-card-title class="text-h5">Are you sure you want to delete this item?</v-card-title>
+                <v-card-actions>
+                    <v-spacer></v-spacer>
+                    <v-btn color="blue darken-1" text @click="closeDelete">Cancel</v-btn>
+                    <v-btn color="blue darken-1" text @click="deleteItemConfirm">Yes</v-btn>
+                    <v-spacer></v-spacer>
+                </v-card-actions>
+                </v-card>
+
+            </v-dialog>
+            </v-toolbar>
+        </template>
+
+            <template v-slot:item.actions="{ item }">
+                <v-icon
+                    small
+                    class="mr-2"
+                    @click="editItem(item)"
+                >
+                    mdi-pencil
+                </v-icon>
+                <v-icon
+                    small
+                    @click="deleteItem(item)"
+                >
+                    mdi-delete
+                </v-icon>
+                </template>
+            </template>
+
             <template v-slot:item.issued="{ item }">
                 <v-simple-checkbox
                     v-model="item.issued"
                     disabled
-                ><v-simple-checkbox>
+                ></v-simple-checkbox>
             </template>
             <template v-slot:item.on_hand="{ item }">
                 <v-simple-checkbox
                     v-model="item.on_hand"
                     disabled
-                ><v-simple-checkbox>
+                ></v-simple-checkbox>
             </template>
             <template v-slot:item.on_location="{ item }">
                 <v-simple-checkbox
                     v-model="item.on_location"
                     disabled
-                ><v-simple-checkbox>
+                ></v-simple-checkbox>
             </template>
             <template v-slot:item.time_checked="{ item }">
                 <v-chip
@@ -65,20 +371,51 @@ Vue.component('computer-table', {
         </v-data-table>
     </div>
     `,
-    methods: {
-        formatDate(value) {
-            return moment(value).fromNow()
-        },
-        getColor(time) {
-            var timeObj = moment(time);
-            var now = moment();
-            var duration = moment.duration(now.diff(timeObj, 'days'));
-            if (duration > 30) return 'red'
-            if (duration > 15) return 'yellow'
-            else return 'green'
-        }
-    }
+
 })
+
+// Vue.component('app-bar', {
+//     template:`
+// <template>
+//   <v-card class="overflow-hidden">
+//     <v-app-bar
+//       absolute
+//       color="white"
+//       elevate-on-scroll
+//       scroll-target="#scrolling-techniques-7"
+//     >
+//       <v-app-bar-nav-icon></v-app-bar-nav-icon>
+
+//       <v-toolbar-title>Inventory</v-toolbar-title>
+
+//       <v-spacer></v-spacer>
+
+//       <v-btn icon>
+//         <v-icon>mdi-magnify</v-icon>
+//       </v-btn>
+
+//       <v-btn icon>
+//         <v-icon>mdi-heart</v-icon>
+//       </v-btn>
+
+//       <v-btn icon>
+//         <v-icon>mdi-dots-vertical</v-icon>
+//       </v-btn>
+//     </v-app-bar>
+//     <v-sheet
+//       id="scrolling-techniques-7"
+//       class="overflow-y-auto"
+//       max-height="600"
+//     >
+//       <v-container style="height: 60px;">
+//       </v-container>
+//     </v-sheet>
+//   </v-card>
+// </template>
+//     `
+// })
+
+
 var app = new Vue({
     el: '#app',
     vuetify: new Vuetify(),
